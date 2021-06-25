519ac9e5127c1d2cf3728486530834f211a65368
Jared He <66887902+jaredjhe@users.noreply.github.com>
2021-06-25 01:20:41 -0500

    Add dotenv files

--- server/index.js
+++ server/index.js
@@ -1,13 +1,30 @@
+require("dotenv").config();
 const express = require("express");
 const app = express(); // create express app
 const server = require("http").createServer(app);
 const cors = require("cors");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
 
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
 
 app.use(cors());
